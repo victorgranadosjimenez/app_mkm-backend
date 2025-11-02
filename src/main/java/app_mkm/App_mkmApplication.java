ee1f3f70100f63306483f5687dac3bef26fccbeb
Chesny <granadosvictor01@gmail.com>
2025-11-02 10:48:01 +0100

    commit3

--- src/main/java/app_mkm/App_mkmApplication.java
+++ src/main/java/app_mkm/App_mkmApplication.java
@@ -3,10 +3,14 @@ package app_mkm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "app_mkm")
+@EntityScan(basePackages = "app_mkm.entity")
+@EnableJpaRepositories(basePackages = "app_mkm.repository")
 public class App_mkmApplication {
     public static void main(String[] args) {
         SpringApplication.run(App_mkmApplication.class, args);
